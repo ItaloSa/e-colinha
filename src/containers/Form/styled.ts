2a948a8a10865ead681b8103824265988999ee8b
Artur Polo Norte <arturpolonorte@MacBook-Air-de-Artur.local>
2022-10-20 21:42:43 -0300

    feat: :art: improve structure with props theme  in styles

--- src/containers/Form/styled.ts
+++ src/containers/Form/styled.ts
@@ -4,17 +4,17 @@ export const FormContainer = styled.div`
   margin-top: 2rem;
   padding: 3rem 2rem 3rem 2rem;
   border-radius: 1rem 1rem 1rem 1rem;
-  background-color: ${(props) => props.theme.bg};
+  background-color: ${({ theme }) => theme.bg};
 `;
 
 export const Link = styled.a`
-  border: 1px solid #fff;
+  border: 1px solid ${({ theme }) => theme.white};
   border-radius: 50px;
   margin-top: 23px;
   padding: 10px 0;
   line-height: 2rem;
-  background-color: ${(props) => props.theme.white};
+  background-color: ${({ theme }) => theme.white};
   text-align: center;
   text-decoration: none;
-  color: #000;
+  color: ${({ theme }) => theme.black};
 `;
